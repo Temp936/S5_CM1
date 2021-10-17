ab4c307070979f864731e875cb620cc94f38f0f7
Temp936 <38109062+Temp936@users.noreply.github.com>
2021-10-17 15:51:36 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,106 +1,7 @@
 package com.company;
 
-public class List{ ///Arraylist
-    private int size;
-    private Object[] arr;
 
-    List(){
-        this.arr = new Object[1];
-        arr[0] = null;
-        this.size = 1;
-    }
-    List(int size, Object[] arr){
-        this.arr = arr;
-        this.size = arr.length() + 1;
-    }
-
-
-    void add(Object ob){
-        Object[] temp = new Object[this.size + 1];
-        for (int i = 0; i<this.size; i++) temp[i] = this.arr[i];
-        temp[this.size] = ob;
-
-        this.size += 1;
-        this.arr = temp;
-    }
-    void add(Object ob, int index){
-        if (index < 0) index = 0;
-        if (index > this.size-1) index = this.size-1;
-
-        Object[] temp = new Object[this.size + 1];
-        for (int i = 0; i<index; i++) temp[i] = this.arr[i];
-        temp[index] = ob;
-        for (int i = index; i<this.size; i++) temp[i+1] = this.arr[i];
-
-        this.size += 1;
-        this.arr = temp;
-    }
-    Object get(int index){
-        if (index < 0) index = 0;
-        if (index > this.size-1) index = this.size-1;
-
-        return this.arr[index];
-    }
-    Object set(Object ob, int index){
-        if (index < 0) index = 0;
-        if (index > this.size-1) index = this.size-1;
-
-        Object temp = this.arr[index];
-        this.arr[index] = ob;
-        return temp;
-    }
-    void set(Object ob, int index){
-        if (index < 0) index = 0;
-        if (index > this.size-1) index = this.size-1;
-
-        this.arr[index] = ob;
-    }
-    boolean contains(Object ob){
-        boolean t = false;
-        for (int i = 0; i<this.size; i++) if(this.arr[i] == ob)
-        {
-            t = true;
-            break;
-        }
-        return t;
-    }
-    int index_of(Object ob){
-        boolean t = false;
-        int index = -1;
-        for (int i = 0; i<this.size; i++) if(this.arr[i] == ob)
-        {
-            t = true;
-            index = i;
-            break;
-        }
-
-        return index;
-    }
-    int size(){
-        return this.size;
-    }
-    boolean is_empty(){
-        if (this.size==0) return true;
-        else return false;
-    }
-
-    void remove(int index){
-        if (index < 0) index = 0;
-        if (index > this.size-1) index = this.size-1;
-        if(this.size > 0){
-            Object[] temp = new Object[this.size - 1];
-            for (int i = 0; i<index; i++) temp[i] = this.arr[i];
-            for (int i = index; i<this.size-1; i++) temp[i] = this.arr[i+1];
-
-            this.size -= 1;
-            this.arr = temp;
-        }
-    }
-
-}
-
-
-public class Map{
+class Map{
     private int size;
     private List keys;
     private List words;
@@ -143,6 +44,7 @@ public class Map{
         return bydefault;
     }
     public Object remove(Object key){
+        Object temp1 = this.get(key);
         for(int i = 0; i < this.size; i++){
             if (this.keys.get(i) == key) {
                 this.size--;
@@ -152,6 +54,7 @@ public class Map{
                 return temp;
             }
         }
+        return temp1;
 
     }
     public boolean keyContains(Object key){
@@ -163,6 +66,7 @@ public class Map{
         return false;
     }
     public List getKeys(){
+        List temp = new List();
         for (int i = 0; i < this.size; i++){
             temp.add(this.keys.get(i));
         }
@@ -170,6 +74,7 @@ public class Map{
         return temp;
     }
     public List getValues(){
+        List temp = new List();
         for (int i = 0; i < this.size; i++){
             temp.add(this.words.get(i));
         }
@@ -199,6 +104,6 @@ public class Map{
 public class Main {
 
     public static void main(String[] args) {
-	//////
+        //////
     }
 }
